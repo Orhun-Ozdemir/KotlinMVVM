31608396165d199206bedd5a6a002f847071ef09
Orhun Özdemir <orhun.ozdemirrr@gmail.com>
2021-09-04 19:55:58 +0300

    DataBinding Kullanimi

--- app/src/main/java/com/example/mvvmkullanimi/MainActivity.kt
+++ app/src/main/java/com/example/mvvmkullanimi/MainActivity.kt
@@ -2,40 +2,45 @@ package com.example.mvvmkullanimi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.mvvmkullanimi.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var tasarim:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        tasarim=DataBindingUtil.setContentView(this,R.layout.activity_main)
 
-        textViewSonuc.text="0"
 
-        buttonToplama.setOnClickListener {
+        tasarim.textViewSonuc.text="0"
 
-            val alinanSayi1=editTextSayi1.text.toString()
-            val alinanSayi2=editTextSayi2.text.toString()
+        tasarim.buttonToplama.setOnClickListener {
+
+            val alinanSayi1= tasarim.editTextSayi1.text.toString()
+            val alinanSayi2= tasarim.editTextSayi2.text.toString()
 
             val sayi1=alinanSayi1.toInt()
             val sayi2=alinanSayi2.toInt()
 
             val toplam=sayi1+sayi2
 
-            textViewSonuc.text=toplam.toString()
+            tasarim.textViewSonuc.text=toplam.toString()
 
         }
 
 
-        buttonCarpma.setOnClickListener {
-            val alinanSayi1=editTextSayi1.text.toString()
-            val alinanSayi2=editTextSayi2.text.toString()
+        tasarim.buttonCarpma.setOnClickListener {
+            val alinanSayi1= tasarim.editTextSayi1.text.toString()
+            val alinanSayi2= tasarim.editTextSayi2.text.toString()
 
             val sayi1=alinanSayi1.toInt()
             val sayi2=alinanSayi2.toInt()
 
             val toplam=sayi1*sayi2
 
-            textViewSonuc.text=toplam.toString()
+            tasarim.textViewSonuc.text=toplam.toString()
 
         }
 
