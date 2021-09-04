6fb57eaa520aa540bd5feb009f0b25fa21474037
Orhun Özdemir <orhun.ozdemirrr@gmail.com>
2021-09-04 19:38:16 +0300

    Klasik Yöntem MainActivity

--- app/src/main/java/com/example/mvvmkullanimi/MainActivity.kt
+++ app/src/main/java/com/example/mvvmkullanimi/MainActivity.kt
@@ -2,11 +2,45 @@ package com.example.mvvmkullanimi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textViewSonuc.text="0"
+
+        buttonToplama.setOnClickListener {
+
+            val alinanSayi1=editTextSayi1.text.toString()
+            val alinanSayi2=editTextSayi2.text.toString()
+
+            val sayi1=alinanSayi1.toInt()
+            val sayi2=alinanSayi2.toInt()
+
+            val toplam=sayi1+sayi2
+
+            textViewSonuc.text=toplam.toString()
+
+        }
+
+
+        buttonCarpma.setOnClickListener {
+            val alinanSayi1=editTextSayi1.text.toString()
+            val alinanSayi2=editTextSayi2.text.toString()
+
+            val sayi1=alinanSayi1.toInt()
+            val sayi2=alinanSayi2.toInt()
+
+            val toplam=sayi1*sayi2
+
+            textViewSonuc.text=toplam.toString()
+
+        }
+
     }
+
+
 }
 
